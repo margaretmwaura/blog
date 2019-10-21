0f9c08159f963996dadd44fbf6e71966b5c23c76
margaretmwaura <mwauramargaret1@gmail.com>
2019-10-21 06:08:55 +0300

    fixed the navbar issue

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,10 +1,10 @@
 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/pages.index">Home <span class="sr-only"></span></a>
+            <a class="nav-link" href="/lsapp/public/">Home <span class="sr-only"></span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/pages.about">Link</a>
+            <a class="nav-link" href="/lsapp/public/about">Link</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
